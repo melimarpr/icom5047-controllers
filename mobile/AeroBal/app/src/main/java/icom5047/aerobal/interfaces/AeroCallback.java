42de667eeed5e5064a46612e04e20e9e6889ee1d
Melisa Marquez <melisamarpr@gmail.com>
2014-05-07 18:00:54 -0400

    Remove Beta Verisons of Code

--- mobile/AeroBal/app/src/main/java/icom5047/aerobal/interfaces/AeroCallback.java
+++
@@ -1,8 +0,0 @@
-package icom5047.aerobal.interfaces;
-
-import java.util.Map;
-
-public interface AeroCallback {
-	void callback(Map<String, Object> objectMap);
-
-}
--- mobile/AeroBal/src/icom5047/aerobal/dialog/OpenDialog.java
+++
@@ -1,7 +0,0 @@
-package icom5047.aerobal.dialog;
-
-import android.support.v4.app.DialogFragment;
-
-public class OpenDialog extends DialogFragment {
-
-}
